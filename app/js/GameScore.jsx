ace9c3fed27c318983bdc70f99bbecbada3737bd
Harri Lehtola <peruukki@hotmail.com>
2015-11-13 22:58:11 +0100

    Show if the game ended in overtime or shootout

--- app/js/GameScore.jsx
+++ app/js/GameScore.jsx
@@ -1,26 +1,42 @@
 const React = require('react');
 
 const GameScore = React.createClass({
+
   propTypes: {
     scores: React.PropTypes.object.isRequired,
     teams: React.PropTypes.object.isRequired
   },
+
+  getDelimiter() {
+    const periodElement = label => <span className="period">{label}</span>;
+
+    if (this.props.scores.overtime) {
+      return periodElement('OT');
+    } else if (this.props.scores.shootout) {
+      return periodElement('SO');
+    } else {
+      return '–';
+    }
+  },
+
   render() {
     const awayTeam = this.props.teams.away;
     const homeTeam = this.props.teams.home;
+
     return (
       <div className="game">
         <div className="teamPanel away">
           <span className="teamName">{awayTeam}</span>
           <span className="teamScore">{this.props.scores[awayTeam]}</span>
         </div>
-        <div className="delimiter">–</div>
+        <div className="delimiter">{this.getDelimiter()}</div>
         <div className="teamPanel home">
           <span className="teamScore">{this.props.scores[homeTeam]}</span>
           <span className="teamName">{homeTeam}</span>
         </div>
       </div>);
   }
+
 });
 
 module.exports = GameScore;
