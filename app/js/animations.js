aa8aefc20e8747cdd0223aaf5c4ef3a472ccfa7f
Harri Lehtola <peruukki@hotmail.com>
2017-11-05 18:14:54 +0200

    Show team names in bold and scores in light gray

--- app/js/animations.js
+++ app/js/animations.js
@@ -7,7 +7,7 @@ function highlightGoalCountChange(classModifier, gameIndex) {
   const element = document.querySelectorAll(`.team-panel--${classModifier} > .team-panel__team-score`)[gameIndex];
   if (element) {
     const highlightDuration = 1000;
-    element.animate([{ color: 'black' }, { color: 'white' }], highlightDuration);
+    element.animate([{ color: 'black' }, { color: '#b5b5b5' }], highlightDuration);
   }
 }
 
