d880ac5c9b8e622a12035f51dad43735422b789f
Harri Lehtola <peruukki@hotmail.com>
2021-04-18 19:36:37 +0300

    Show games in different states separately in gallery
    This will help when adding transitions between states later.

--- gallery/js/app.js
+++ gallery/js/app.js
@@ -1,4 +1,4 @@
-import { div } from '@cycle/dom';
+import { div, span } from '@cycle/dom';
 import xs from 'xstream';
 
 import renderGame from '../../app/js/components/game';
@@ -10,77 +10,92 @@ import {
   GAME_DISPLAY_PRE_GAME,
   GAME_STATE_FINISHED,
   GAME_STATE_IN_PROGRESS,
-  PLAYBACK_FINISHED,
-  PLAYBACK_IN_PROGRESS,
-  PLAYBACK_NOT_STARTED,
 } from '../../app/js/events/constants';
 import scoresAllRegularTime from '../../test/data/latest.json';
 
 export default function main() {
-  return () => ({ DOM: view(model()) });
+  return () => ({ DOM: view(model(intent())) });
 }
 
-function model() {
+function intent() {
   const progress = {
     currentPeriod: 3,
     currentPeriodOrdinal: '3rd',
     currentPeriodTimeRemaining: { pretty: '00:05', min: 0, sec: 5 },
   };
-  const stateCombinations = [
-    [
-      'Playback not started',
-      PLAYBACK_NOT_STARTED,
-      GAME_DISPLAY_PRE_GAME,
-      { state: GAME_STATE_IN_PROGRESS, progress },
-    ],
-    [
-      'Playback in progress, hasn’t passed game’s current progress',
-      PLAYBACK_IN_PROGRESS,
-      GAME_DISPLAY_PLAYBACK,
-      { state: GAME_STATE_FINISHED },
-    ],
-    [
-      'Playback in progress, has passed game’s current progress',
-      PLAYBACK_IN_PROGRESS,
-      GAME_DISPLAY_IN_PROGRESS,
-      { state: GAME_STATE_IN_PROGRESS, progress },
-    ],
-    [
-      'Playback finished, game still in progress',
-      PLAYBACK_FINISHED,
-      GAME_DISPLAY_POST_GAME_IN_PROGRESS,
-      { state: GAME_STATE_IN_PROGRESS, progress },
-    ],
-    [
-      'Playback finished, game finished',
-      PLAYBACK_FINISHED,
-      GAME_DISPLAY_POST_GAME_FINISHED,
-      { state: GAME_STATE_FINISHED },
-    ],
+  return [
+    {
+      game: {
+        description: 'Game in progress',
+        status: { state: GAME_STATE_IN_PROGRESS, progress },
+      },
+      states: [
+        {
+          description: 'Playback not started',
+          gameDisplay: GAME_DISPLAY_PRE_GAME,
+        },
+        {
+          description: 'Playback has passed game’s current progress',
+          gameDisplay: GAME_DISPLAY_IN_PROGRESS,
+        },
+        {
+          description: 'Playback finished',
+          gameDisplay: GAME_DISPLAY_POST_GAME_IN_PROGRESS,
+        },
+      ],
+    },
+    {
+      game: {
+        description: 'Game finished',
+        status: { state: GAME_STATE_FINISHED },
+      },
+      states: [
+        {
+          description: 'Playback not started',
+          gameDisplay: GAME_DISPLAY_PRE_GAME,
+        },
+        {
+          description: 'Playback in progress',
+          gameDisplay: GAME_DISPLAY_PLAYBACK,
+        },
+        {
+          description: 'Playback finished',
+          gameDisplay: GAME_DISPLAY_POST_GAME_FINISHED,
+        },
+      ],
+    },
   ];
-  const game = scoresAllRegularTime.games[1];
-  const games$ = xs.of(
-    stateCombinations.map(([description, playbackState, gameDisplay, status], index) => ({
-      description,
-      playbackState,
-      gameDisplay,
-      gameState: { ...game, status },
-      currentGoals: game.goals.slice(0, index % 2 === 0 ? -1 : undefined),
-    }))
-  );
-  return { games$ };
 }
 
-function view({ games$ }) {
-  return games$.map(games => {
-    return div(
+function model(gameStates) {
+  const gameData = scoresAllRegularTime.games[1];
+  const gameStates$ = xs.of(
+    gameStates.map(({ game, states }, index) => ({
+      gameDescription: game.description,
+      games: states.map(state => ({
+        description: state.description,
+        gameDisplay: state.gameDisplay,
+        gameState: { ...gameData, status: game.status },
+        currentGoals: gameData.goals.slice(0, index % 2 === 0 ? -1 : undefined),
+      })),
+    }))
+  );
+  return { gameStates$ };
+}
+
+function view({ gameStates$ }) {
+  return gameStates$.map(gameStates =>
+    div(
       '.score-list',
-      games.map(game =>
-        div('.gallery-game', [
-          div('.gallery-game__description', [game.description]),
-          renderGame(game.gameDisplay, game.gameState, game.currentGoals),
-        ])
-      )
-    );
-  });
+      gameStates.flatMap(({ gameDescription, games }) => [
+        div('.gallery-heading', span('.gallery-heading__description', gameDescription)),
+        ...games.map(game =>
+          div('.gallery-game', [
+            div('.gallery-game__description', [game.description]),
+            renderGame(game.gameDisplay, game.gameState, game.currentGoals),
+          ])
+        ),
+      ])
+    )
+  );
 }
