5dae1b668ca18164dc18fe0e97d45b904b129d6f
Harri Lehtola <peruukki@hotmail.com>
2021-04-20 19:17:28 +0300

    Remove extra animation for in-progress game when playback finishes
    There was an additional state change GAME_DISPLAY_IN_PROGRESS ->
    GAME_DISPLAY_PLAYBACK before the final GAME_DISPLAY_POST_GAME_IN_PROGRESS,
    causing the game description to disappear briefly, thus resulting in a
    fade-in animation on reappear.

--- app/js/events/game-displays.js
+++ app/js/events/game-displays.js
@@ -8,7 +8,6 @@ import {
   GAME_DISPLAY_PRE_GAME,
   PLAYBACK_FINISHED,
   PLAYBACK_IN_PROGRESS,
-  PLAYBACK_NOT_STARTED,
 } from './constants';
 import {
   hasClockPassedCurrentProgress,
@@ -18,33 +17,30 @@ import {
 } from './utils';
 
 export default function getGameDisplays$(clock$, scores$) {
-  const playbackState$ = clock$
-    .map(clock => (clock.end && !clock.period ? PLAYBACK_FINISHED : PLAYBACK_IN_PROGRESS))
-    .startWith(PLAYBACK_NOT_STARTED);
   const initialgameDisplays$ = scores$
     .filter(scores => scores.games.length > 0)
     .map(scores => Array.from({ length: scores.games.length }, () => GAME_DISPLAY_PRE_GAME));
-  const gameDisplays$ = xs
-    .combine(scores$, clock$, playbackState$)
-    .map(([scores, clock, playbackState]) =>
-      scores.games.map(game => {
-        if (playbackState === PLAYBACK_NOT_STARTED || !hasGameStarted(game.status.state)) {
-          return GAME_DISPLAY_PRE_GAME;
-        }
-        if (
-          isGameInProgress(game.status.state) &&
-          hasClockPassedCurrentProgress(clock, game.status)
-        ) {
-          return GAME_DISPLAY_IN_PROGRESS;
-        }
-        if (playbackState === PLAYBACK_FINISHED) {
-          return hasGameFinished(game.status.state)
-            ? GAME_DISPLAY_POST_GAME_FINISHED
-            : GAME_DISPLAY_POST_GAME_IN_PROGRESS;
-        }
-        return GAME_DISPLAY_PLAYBACK;
-      })
-    );
+  const gameDisplays$ = xs.combine(scores$, clock$).map(([scores, clock]) => {
+    const playbackState = clock.end && !clock.period ? PLAYBACK_FINISHED : PLAYBACK_IN_PROGRESS;
+    return scores.games.map(game => {
+      if (!hasGameStarted(game.status.state)) {
+        return GAME_DISPLAY_PRE_GAME;
+      }
+      if (
+        playbackState === PLAYBACK_IN_PROGRESS &&
+        isGameInProgress(game.status.state) &&
+        hasClockPassedCurrentProgress(clock, game.status)
+      ) {
+        return GAME_DISPLAY_IN_PROGRESS;
+      }
+      if (playbackState === PLAYBACK_FINISHED) {
+        return hasGameFinished(game.status.state)
+          ? GAME_DISPLAY_POST_GAME_FINISHED
+          : GAME_DISPLAY_POST_GAME_IN_PROGRESS;
+      }
+      return GAME_DISPLAY_PLAYBACK;
+    });
+  });
   const gameDisplaysWithInitialValues$ = xs.merge(initialgameDisplays$, gameDisplays$);
   return gameDisplaysWithInitialValues$;
 }
