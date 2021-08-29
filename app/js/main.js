69c301ce0cdca5b4477c0611a648b548619164a4
Harri Lehtola <peruukki@hotmail.com>
2021-08-29 12:51:59 +0300

    Add service worker registration and build setup

--- app/js/main.js
+++ app/js/main.js
@@ -3,8 +3,11 @@ import { makeDOMDriver } from '@cycle/dom';
 import { makeHTTPDriver } from '@cycle/http';
 
 import app from './components/app';
+import registerServiceWorker from './service-worker/register-sw';
 import animations from './utils/animations';
 import { fetchTeamLogoSVGSymbols } from './utils/logos';
+
+registerServiceWorker();
 
 fetchTeamLogoSVGSymbols();
 
---
+++ app/js/service-worker/register-sw.js
@@ -0,0 +1,9 @@
+const registerServiceWorker = () => {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js');
+    });
+  }
+};
+
+export default registerServiceWorker;
