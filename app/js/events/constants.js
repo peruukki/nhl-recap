366d85be14d637dc56d5e975c7ee5a748cdedc96
Harri Lehtola <peruukki@hotmail.com>
2022-04-10 14:51:07 +0300

    Add descriptive comments to game display constants

--- app/js/events/constants.js
+++ app/js/events/constants.js
@@ -1,10 +1,15 @@
 export const ERROR_MISSING_ALL_GOALS = 'MISSING-ALL-GOALS';
 export const ERROR_SCORE_AND_GOAL_COUNT_MISMATCH = 'SCORE-AND-GOAL-COUNT-MISMATCH';
 
+/** Showing pre-game information, before playback has started. */
 export const GAME_DISPLAY_PRE_GAME = 'GAME_DISPLAY_PRE_GAME';
+/** Playback has started, clock hasn't reached this game's end time. */
 export const GAME_DISPLAY_PLAYBACK = 'GAME_DISPLAY_PLAYBACK';
+/** Playback has started, clock has reached this in-progress game's current time. */
 export const GAME_DISPLAY_IN_PROGRESS = 'GAME_DISPLAY_IN_PROGRESS';
+/** Playback has finished, showing this finished game's post-game information. */
 export const GAME_DISPLAY_POST_GAME_FINISHED = 'GAME_DISPLAY_POST_GAME_FINISHED';
+/** Playback has finished, showing this in-progress game's post-game information. */
 export const GAME_DISPLAY_POST_GAME_IN_PROGRESS = 'GAME_DISPLAY_POST_GAME_IN_PROGRESS';
 
 export const GAME_STATE_FINISHED = 'FINAL';
