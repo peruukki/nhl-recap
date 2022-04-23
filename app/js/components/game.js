7fd71e39f5f9b7bf65b1b752d8949a89a104ab51
Harri Lehtola <peruukki@hotmail.com>
2022-04-23 19:46:32 +0300

    Extract score panel to its own component

--- app/js/components/game.js
+++ app/js/components/game.js
@@ -12,12 +12,11 @@ import {
   GAME_STATE_IN_PROGRESS,
   GAME_STATE_NOT_STARTED,
   GAME_STATE_POSTPONED,
-  PERIOD_OVERTIME,
   PERIOD_SHOOTOUT,
 } from '../events/constants';
-import { renderTeamLogo } from '../utils/logos';
 import { truncatePlayerName } from '../utils/utils';
 import { renderPeriodNumber, renderTime } from './clock';
+import ScorePanel from './score-panel';
 import GameStats from './stats/game-stats';
 import TeamStats from './stats/team-stats';
 
@@ -57,7 +56,7 @@ export default function renderGame(
   );
   return div('.game-container', [
     div(`.game.expand--${gameAnimationIndex}`, { class: { [`game--${gameDisplay}`]: true } }, [
-      renderScorePanel(teams, awayGoals, homeGoals, period, isBeforeGame),
+      ScorePanel(teams, awayGoals, homeGoals, period, isBeforeGame),
       renderInfoPanel({
         showGameStats,
         showPreGameStats,
@@ -99,42 +98,6 @@ function getPlayoffSeriesWinsAfterGame(seriesWins, teams, awayGoals, homeGoals) 
       ? { [teams.away.abbreviation]: seriesWins[teams.away.abbreviation] + 1 }
       : { [teams.home.abbreviation]: seriesWins[teams.home.abbreviation] + 1 };
   return _.merge({}, seriesWins, updatedWinCount);
-}
-
-function renderScorePanel(teams, awayGoals, homeGoals, period, isBeforeGame) {
-  const scoreVisibilityClass = isBeforeGame ? '.team-panel__team-score--hidden' : '.fade-in';
-  const delimiterVisibilityClass = isBeforeGame ? '' : '.fade-in';
-  return div('.game__score-panel', [
-    div('.team-panel.team-panel--away', [
-      renderLogo(teams.away.id, 'away'),
-      span('.team-panel__team-name', teams.away.abbreviation),
-      span(`.team-panel__team-score${scoreVisibilityClass}`, [awayGoals.length]),
-    ]),
-    div(
-      `.team-panel__delimiter${delimiterVisibilityClass}`,
-      isBeforeGame ? 'at' : renderDelimiter(period)
-    ),
-    div('.team-panel.team-panel--home', [
-      span(`.team-panel__team-score${scoreVisibilityClass}`, [homeGoals.length]),
-      span('.team-panel__team-name', teams.home.abbreviation),
-      renderLogo(teams.home.id, 'home'),
-    ]),
-  ]);
-}
-
-function renderLogo(teamId, modifier) {
-  return span('.team-logo', [
-    renderTeamLogo(
-      teamId,
-      `team-logo__image team-logo__image--${modifier} team-logo__image--${teamId}`
-    ),
-  ]);
-}
-
-function renderDelimiter(period) {
-  return period === PERIOD_OVERTIME || period === PERIOD_SHOOTOUT || period > 3
-    ? span('.team-panel__delimiter-period', period === PERIOD_SHOOTOUT ? 'SO' : 'OT')
-    : '';
 }
 
 function renderInfoPanel({
---
+++ app/js/components/score-panel.js
@@ -0,0 +1,40 @@
+import { div, span } from '@cycle/dom';
+
+import { PERIOD_OVERTIME, PERIOD_SHOOTOUT } from '../events/constants';
+import { renderTeamLogo } from '../utils/logos';
+
+export default function ScorePanel(teams, awayGoals, homeGoals, period, isBeforeGame) {
+  const scoreVisibilityClass = isBeforeGame ? '.team-panel__team-score--hidden' : '.fade-in';
+  const delimiterVisibilityClass = isBeforeGame ? '' : '.fade-in';
+  return div('.game__score-panel', [
+    div('.team-panel.team-panel--away', [
+      renderLogo(teams.away.id, 'away'),
+      span('.team-panel__team-name', teams.away.abbreviation),
+      span(`.team-panel__team-score${scoreVisibilityClass}`, [awayGoals.length]),
+    ]),
+    div(
+      `.team-panel__delimiter${delimiterVisibilityClass}`,
+      isBeforeGame ? 'at' : renderDelimiter(period)
+    ),
+    div('.team-panel.team-panel--home', [
+      span(`.team-panel__team-score${scoreVisibilityClass}`, [homeGoals.length]),
+      span('.team-panel__team-name', teams.home.abbreviation),
+      renderLogo(teams.home.id, 'home'),
+    ]),
+  ]);
+}
+
+function renderLogo(teamId, modifier) {
+  return span('.team-logo', [
+    renderTeamLogo(
+      teamId,
+      `team-logo__image team-logo__image--${modifier} team-logo__image--${teamId}`
+    ),
+  ]);
+}
+
+function renderDelimiter(period) {
+  return period === PERIOD_OVERTIME || period === PERIOD_SHOOTOUT || period > 3
+    ? span('.team-panel__delimiter-period', period === PERIOD_SHOOTOUT ? 'SO' : 'OT')
+    : '';
+}
