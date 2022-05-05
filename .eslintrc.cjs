85a19d3ecece530577b47924694257477d632887
Harri Lehtola <peruukki@hotmail.com>
2022-05-05 20:40:46 +0300

    Update ESLint packages to latest versions

---
+++ .eslintrc.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  env: {
+    browser: true,
+    es6: true,
+    mocha: true,
+  },
+  globals: {
+    process: false,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  overrides: [
+    {
+      files: 'animations.js',
+      rules: {
+        'no-param-reassign': 'off',
+      },
+    },
+  ],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'no-console': 'warn',
+    'no-nested-ternary': 'off',
+    'no-unused-vars': 'warn',
+    'no-restricted-exports': 'off',
+    'no-restricted-globals': 'off',
+    'no-use-before-define': 'off',
+    semi: 'off',
+  },
+  settings: {
+    'import/resolver': { node: { paths: ['.'] } },
+  },
+};
