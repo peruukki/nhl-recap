2de534855736433b8af96edbb6ce096291219b71
Harri Lehtola <peruukki@hotmail.com>
2022-05-08 21:37:28 +0300

    Proper TypeScript setup for ESLint
    The TypeScript parser complained about .cjs or .js file suffix, so the
    configuration file is .eslintrc again.

--- test/test-animations.js
+++ test/test-animations.js
@@ -1,4 +1,4 @@
-// eslint-disable-next-line no-unused-vars
+// eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
 function highlightGoal(classModifier, gameIndex) {}
 
 export default { highlightGoal };
