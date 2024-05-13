22dcafe20482cb7b59794d75de5b1795a2deb17c
Harri Lehtola <peruukki@hotmail.com>
2024-05-13 09:01:32 +0300

    Set empty alt on decorative team logo images

--- app/src/utils/logos.ts
+++ app/src/utils/logos.ts
@@ -3,6 +3,7 @@ import { img, VNode } from '@cycle/dom';
 export function renderTeamLogoSVG(teamAbbreviation: string, className: string): VNode {
   return img({
     attrs: {
+      alt: '',
       class: className,
       src: `https://assets.nhle.com/logos/nhl/svg/${teamAbbreviation}_dark.svg`,
     },
