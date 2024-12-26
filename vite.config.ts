ec3a20068c5b395051b1f7454473a064b1e2af41
Harri Lehtola <peruukki@hotmail.com>
2024-12-26 10:01:57 +0200

    Major upgrade Vite related packages to latest

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import autoprefixer from 'autoprefixer';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   base: '/nhl-recap/',
