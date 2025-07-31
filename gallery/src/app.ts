dfb19356ed1ab737341dc2340515e2456835ee3f
Harri Lehtola <peruukki@hotmail.com>
2025-07-31 20:03:34 +0300

    Extract gallery game data to its own file

--- gallery/src/app.ts
+++ gallery/src/app.ts
@@ -4,8 +4,8 @@ import dropRepeats from 'xstream/extra/dropRepeats';
 import fromEvent from 'xstream/extra/fromEvent';
 
 import Game from '../../app/src/components/game';
-import { scoresAllRegularTime, scoresAllRegularTimePlayoffs } from '../../app/src/test/data';
-import type { GameDisplay, GameStats, GameStatus, Game as GameT, Goal } from '../../app/src/types';
+import type { GameDisplay, GameStats, Game as GameT, Goal } from '../../app/src/types';
+import { gamesData, stateDefinitions } from './data';
 import { getSectionExpandedState, setSectionExpandedState } from './storage';
 
 type Sources = {
@@ -19,7 +19,6 @@ type Sinks = {
 type Actions = {
   expandCollapseAll$: Stream<'expandAll' | 'collapseAll'>;
   expandCollapseSingle$: Stream<Event>;
-  stateDefinitions: GameStateDefinition[];
 };
 
 type State = {
@@ -35,19 +34,6 @@ type GalleryGameT = {
     status: string;
     gameStats: GameStats;
   };
-};
-
-type GameStateDefinition = {
-  gameStatus: {
-    description: string;
-    gameStats?: undefined;
-    status: GameStatus;
-  };
-  states: ({
-    description: string;
-    gameDisplays: string[];
-    goalCount: number;
-  } | null)[];
 };
 
 export default function main(): (sources: Sources) => Sinks {
@@ -67,119 +53,13 @@ function intent(DOM: Sources['DOM']): Actions {
     .map((elements) => xs.merge(...elements.map((element) => fromEvent(element, 'toggle'))))
     .flatten();
 
-  const progress = {
-    currentPeriod: 3,
-    currentPeriodOrdinal: '3rd',
-    currentPeriodTimeRemaining: { pretty: '00:05', min: 0, sec: 5 },
-  };
   return {
     expandCollapseAll$,
     expandCollapseSingle$,
-    stateDefinitions: [
-      {
-        gameStatus: {
-          description: 'in progress',
-          status: { state: 'LIVE', progress },
-        },
-        states: [
-          {
-            description: 'Playback not started',
-            gameDisplays: ['pre-game', 'pre-game', 'pre-game', 'pre-game'],
-            goalCount: 4,
-          },
-          {
-            description: 'Playback has passed game’s current progress',
-            gameDisplays: ['playback', 'in-progress', 'in-progress', 'in-progress'],
-            goalCount: 4,
-          },
-          {
-            description: 'Playback finished',
-            gameDisplays: [
-              'in-progress',
-              'pre-summary-in-progress',
-              'summary-in-progress',
-              'post-game-in-progress',
-            ],
-            goalCount: 4,
-          },
-        ],
-      },
-      {
-        gameStatus: {
-          description: 'finished',
-          status: { state: 'FINAL' },
-        },
-        states: [
-          {
-            description: 'Playback not started',
-            gameDisplays: ['pre-game', 'pre-game', 'pre-game', 'pre-game'],
-            goalCount: 5,
-          },
-          {
-            description: 'Playback in progress',
-            gameDisplays: ['pre-game', 'playback', 'playback', 'playback'],
-            goalCount: 0,
-          },
-          {
-            description: 'Playback finished',
-            gameDisplays: [
-              'playback',
-              'pre-summary-finished',
-              'summary-finished',
-              'post-game-finished',
-            ],
-            goalCount: 5,
-          },
-        ],
-      },
-      {
-        gameStatus: {
-          description: 'not started',
-          status: { state: 'PREVIEW' },
-          gameStats: undefined,
-        },
-        states: [
-          {
-            description: 'Playback in any state',
-            gameDisplays: ['pre-game', 'pre-game', 'pre-game', 'pre-game'],
-            goalCount: 0,
-          },
-          null,
-          null,
-        ],
-      },
-    ],
   };
 }
 
-function model({ expandCollapseAll$, expandCollapseSingle$, stateDefinitions }: Actions): State {
-  const gamesData = [
-    { description: 'Regular season game', data: scoresAllRegularTime.games[1] },
-    {
-      description: 'Playoff game',
-      data: scoresAllRegularTimePlayoffs.games[1],
-    },
-    {
-      description: 'Regular season game with errors',
-      data: {
-        ...scoresAllRegularTime.games[1],
-        errors: [
-          { error: 'MISSING-ALL-GOALS' },
-          { error: 'SCORE-AND-GOAL-COUNT-MISMATCH', details: { goalCount: 3, scoreCount: 5 } },
-        ],
-      },
-    },
-    {
-      description: 'Playoff game with error',
-      data: {
-        ...scoresAllRegularTimePlayoffs.games[1],
-        errors: [
-          { error: 'SCORE-AND-GOAL-COUNT-MISMATCH', details: { goalCount: 3, scoreCount: 5 } },
-        ],
-      },
-    },
-  ];
-
+function model({ expandCollapseAll$, expandCollapseSingle$ }: Actions): State {
   const initialSectionExpandedStates = Array(stateDefinitions.length * gamesData.length)
     .fill(null)
     .map((_, index) => getSectionExpandedState(index));
---
+++ gallery/src/data.ts
@@ -0,0 +1,123 @@
+import type { GameStatus } from '../../app/src/types';
+import { scoresAllRegularTime, scoresAllRegularTimePlayoffs } from '../../app/src/test/data';
+
+type GameStateDefinition = {
+  gameStatus: {
+    description: string;
+    gameStats?: undefined;
+    status: GameStatus;
+  };
+  states: ({
+    description: string;
+    gameDisplays: string[];
+    goalCount: number;
+  } | null)[];
+};
+
+const progress = {
+  currentPeriod: 3,
+  currentPeriodOrdinal: '3rd',
+  currentPeriodTimeRemaining: { pretty: '00:05', min: 0, sec: 5 },
+};
+
+export const stateDefinitions: GameStateDefinition[] = [
+  {
+    gameStatus: {
+      description: 'in progress',
+      status: { state: 'LIVE', progress },
+    },
+    states: [
+      {
+        description: 'Playback not started',
+        gameDisplays: ['pre-game', 'pre-game', 'pre-game', 'pre-game'],
+        goalCount: 4,
+      },
+      {
+        description: 'Playback has passed game’s current progress',
+        gameDisplays: ['playback', 'in-progress', 'in-progress', 'in-progress'],
+        goalCount: 4,
+      },
+      {
+        description: 'Playback finished',
+        gameDisplays: [
+          'in-progress',
+          'pre-summary-in-progress',
+          'summary-in-progress',
+          'post-game-in-progress',
+        ],
+        goalCount: 4,
+      },
+    ],
+  },
+  {
+    gameStatus: {
+      description: 'finished',
+      status: { state: 'FINAL' },
+    },
+    states: [
+      {
+        description: 'Playback not started',
+        gameDisplays: ['pre-game', 'pre-game', 'pre-game', 'pre-game'],
+        goalCount: 5,
+      },
+      {
+        description: 'Playback in progress',
+        gameDisplays: ['pre-game', 'playback', 'playback', 'playback'],
+        goalCount: 0,
+      },
+      {
+        description: 'Playback finished',
+        gameDisplays: [
+          'playback',
+          'pre-summary-finished',
+          'summary-finished',
+          'post-game-finished',
+        ],
+        goalCount: 5,
+      },
+    ],
+  },
+  {
+    gameStatus: {
+      description: 'not started',
+      status: { state: 'PREVIEW' },
+      gameStats: undefined,
+    },
+    states: [
+      {
+        description: 'Playback in any state',
+        gameDisplays: ['pre-game', 'pre-game', 'pre-game', 'pre-game'],
+        goalCount: 0,
+      },
+      null,
+      null,
+    ],
+  },
+];
+
+export const gamesData = [
+  { description: 'Regular season game', data: scoresAllRegularTime.games[1] },
+  {
+    description: 'Playoff game',
+    data: scoresAllRegularTimePlayoffs.games[1],
+  },
+  {
+    description: 'Regular season game with errors',
+    data: {
+      ...scoresAllRegularTime.games[1],
+      errors: [
+        { error: 'MISSING-ALL-GOALS' },
+        { error: 'SCORE-AND-GOAL-COUNT-MISMATCH', details: { goalCount: 3, scoreCount: 5 } },
+      ],
+    },
+  },
+  {
+    description: 'Playoff game with error',
+    data: {
+      ...scoresAllRegularTimePlayoffs.games[1],
+      errors: [
+        { error: 'SCORE-AND-GOAL-COUNT-MISMATCH', details: { goalCount: 3, scoreCount: 5 } },
+      ],
+    },
+  },
+];
