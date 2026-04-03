69ee56b5364ca9eb93f7890445ce3bd43be614fe
Harri Lehtola <peruukki@hotmail.com>
2026-04-03 09:29:02 +0300

    Use node protocol for Node.js modules in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
 import autoprefixer from 'autoprefixer';
-import { dirname, resolve } from 'path';
-import { fileURLToPath } from 'url';
 import { defineConfig } from 'vitest/config';
 
 const __filename = fileURLToPath(import.meta.url);
