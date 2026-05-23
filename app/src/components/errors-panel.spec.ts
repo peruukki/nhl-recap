29ae3c29d651a9b32eab508ef8726d6b3a95da37
Harri Lehtola <peruukki@hotmail.com>
2026-05-23 09:06:05 +0300

    Refactor assertErrors helper in the errors panel test file
    Use an object for parameters to make assertions clearer, make state
    parameter non-optional, and pass expected values explicitly.

--- app/src/components/errors-panel.spec.ts
+++ app/src/components/errors-panel.spec.ts
@@ -9,44 +9,65 @@ import { getGameCard } from './test-utils';
 
 describe('errors panel', () => {
   it('should not show errors with valid data', () => {
-    assertErrors(undefined);
+    assertErrors({ gameErrors: undefined, expectedErrorTexts: undefined, state: 'FINAL' });
   });
 
   it('should show appropriate error when all goal data is missing', () => {
-    assertErrors([{ error: 'MISSING-ALL-GOALS' }]);
+    assertErrors({
+      gameErrors: [{ error: 'MISSING-ALL-GOALS' }],
+      expectedErrorTexts: ['Missing all goal data'],
+      state: 'FINAL',
+    });
   });
 
   it("should show appropriate error when some goals' data is missing", () => {
-    assertErrors([
-      { error: 'SCORE-AND-GOAL-COUNT-MISMATCH', details: { goalCount: 2, scoreCount: 3 } },
-    ]);
+    assertErrors({
+      gameErrors: [
+        { error: 'SCORE-AND-GOAL-COUNT-MISMATCH', details: { goalCount: 2, scoreCount: 3 } },
+      ],
+      expectedErrorTexts: ['Missing 1 goal from data'],
+      state: 'FINAL',
+    });
   });
 
   it("should show appropriate error when too many goals' data exists", () => {
-    assertErrors([
-      { error: 'SCORE-AND-GOAL-COUNT-MISMATCH', details: { goalCount: 2, scoreCount: 1 } },
-    ]);
+    assertErrors({
+      gameErrors: [
+        { error: 'SCORE-AND-GOAL-COUNT-MISMATCH', details: { goalCount: 2, scoreCount: 1 } },
+      ],
+      expectedErrorTexts: ['1 too many goals in data'],
+      state: 'FINAL',
+    });
   });
 
   it('should show appropriate pending goal error when game state is live', () => {
-    assertErrors(
-      [{ error: 'SCORE-AND-GOAL-COUNT-MISMATCH', details: { goalCount: 3, scoreCount: 2 } }],
-      'LIVE',
-    );
+    assertErrors({
+      gameErrors: [
+        { error: 'SCORE-AND-GOAL-COUNT-MISMATCH', details: { goalCount: 3, scoreCount: 2 } },
+      ],
+      expectedErrorTexts: ['One goal pending'],
+      state: 'LIVE',
+    });
   });
 
   it('should show appropriate unconfirmed goal error when game state is live', () => {
-    assertErrors(
-      [{ error: 'SCORE-AND-GOAL-COUNT-MISMATCH', details: { goalCount: 2, scoreCount: 3 } }],
-      'LIVE',
-    );
+    assertErrors({
+      gameErrors: [
+        { error: 'SCORE-AND-GOAL-COUNT-MISMATCH', details: { goalCount: 2, scoreCount: 3 } },
+      ],
+      expectedErrorTexts: ['Last goal unconfirmed'],
+      state: 'LIVE',
+    });
   });
 
   it('should show regular goal count mismatch error when game state is live and difference is greater than 1', () => {
-    assertErrors(
-      [{ error: 'SCORE-AND-GOAL-COUNT-MISMATCH', details: { goalCount: 4, scoreCount: 2 } }],
-      'LIVE',
-    );
+    assertErrors({
+      gameErrors: [
+        { error: 'SCORE-AND-GOAL-COUNT-MISMATCH', details: { goalCount: 4, scoreCount: 2 } },
+      ],
+      expectedErrorTexts: ['2 too many goals in data'],
+      state: 'LIVE',
+    });
   });
 
   it('should not show errors during playback', () => {
@@ -74,7 +95,15 @@ describe('errors panel', () => {
   });
 });
 
-function assertErrors(gameErrors: StatError[] | undefined, state: GameState = 'FINAL') {
+function assertErrors({
+  gameErrors,
+  expectedErrorTexts,
+  state,
+}: {
+  gameErrors: StatError[] | undefined;
+  expectedErrorTexts: string[] | undefined;
+  state: GameState;
+}) {
   const status: GameStatus =
     state === 'LIVE'
       ? {
@@ -98,7 +127,7 @@ function assertErrors(gameErrors: StatError[] | undefined, state: GameState = 'F
       0,
     ),
   );
-  const expected = expectedErrorsPanel(gameErrors, state, true);
+  const expected = expectedErrorsPanel(expectedErrorTexts, true);
   expect(errorsPanel).toEqual(expected);
 }
 
@@ -106,38 +135,13 @@ function getErrorsPanel(vtree: VNode) {
   return getGameCard(vtree)?.children?.[3];
 }
 
-function expectedErrorsPanel(errors: StatError[] | undefined, state: GameState, show: boolean) {
-  return errors
+function expectedErrorsPanel(errorTexts: string[] | undefined, show: boolean) {
+  return errorTexts
     ? Expandable({ show }, [
         div(
           '.game__errors',
-          errors.map((error) => div(getErrorText(error, state))),
+          errorTexts.map((errorText) => div(errorText)),
         ),
       ])
     : null;
-}
-
-function getErrorText(error: StatError, state: GameState): string {
-  switch (error.error) {
-    case 'MISSING-ALL-GOALS':
-      return 'Missing all goal data';
-    case 'SCORE-AND-GOAL-COUNT-MISMATCH': {
-      const { goalCount, scoreCount } = error.details;
-      if (state === 'LIVE') {
-        if (goalCount === scoreCount + 1) {
-          return 'One goal pending';
-        }
-        if (scoreCount === goalCount + 1) {
-          return 'Last goal unconfirmed';
-        }
-      }
-      const difference = Math.abs(goalCount - scoreCount);
-      const pluralSuffix = difference === 1 ? '' : 's';
-      return goalCount < scoreCount
-        ? `Missing ${difference} goal${pluralSuffix} from data`
-        : `${difference} too many goals in data`;
-    }
-    default:
-      return `Unknown error ${JSON.stringify(error)}`;
-  }
 }
